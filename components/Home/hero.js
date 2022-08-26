589f6d9061872e0c9870a503e2197eb20be1ad48
kimtaekang <k_2684@naver.com>
2022-08-27 04:42:23 +0900

    chore: add link

--- components/Home/hero.js
+++ components/Home/hero.js
@@ -1,9 +1,9 @@
+import Link from "next/link";
 import Animation from "./animation";
 
 export default function Hero() {
   return (
     <>
-      {" "}
       <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
         <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
           Grayash
@@ -15,9 +15,11 @@ export default function Hero() {
           💻 Beyond Coding(2022.02 ~ )
         </p>
         <div className="flex justify-center">
-          <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-            프로젝트 보러가기
-          </button>
+          <Link href="/projects">
+            <a className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+              프로젝트 보러가기
+            </a>
+          </Link>
         </div>
       </div>
       <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
--- components/footer.js
+++ components/footer.js
@@ -1,12 +1,16 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <>
       <footer className="text-gray-600 body-font">
         <div className="bg-gray-100">
           <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
-            <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-              <span className="ml-3 text-xl">Grayash`s Portfolio</span>
-            </a>
+            <Link href="/">
+              <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+                <span className="ml-3 text-xl">Grayash`s Portfolio</span>
+              </a>
+            </Link>
             <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
               <a href="https://github.com/grayashh" className="text-gray-500">
                 <svg
@@ -40,7 +44,7 @@ export default function Footer() {
             </span>
           </div>
         </div>
-      </footer>{" "}
+      </footer>
     </>
   );
 }
