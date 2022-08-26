c4c9b86d4029fd25125044d474592cdf8d940e1f
kimtaekang <k_2684@naver.com>
2022-08-27 03:24:29 +0900

    feat: add lottie in main page

---
+++ components/Home/hero.js
@@ -0,0 +1,28 @@
+import Animation from "./animation";
+
+export default function Hero() {
+  return (
+    <>
+      {" "}
+      <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+          Grayash
+          <br className="hidden lg:inline-block" />
+          Develope Portfolio
+        </h1>
+        <p className="mb-8 leading-relaxed">
+          👨‍🎓 MJU Convergence Software Data Technology (2022.03 ~ ) <br />
+          💻 Beyond Coding(2022.02 ~ )
+        </p>
+        <div className="flex justify-center">
+          <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            프로젝트 보러가기
+          </button>
+        </div>
+      </div>
+      <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+        <Animation />
+      </div>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Hero from "../components/Home/hero";
 import Layout from "../components/layout";
 
 export default function Home() {
@@ -12,25 +13,9 @@ export default function Home() {
       </Head>
       <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              Grayash
-              <br className="hidden lg:inline-block" />
-              Develope Portfolio
-            </h1>
-            <p className="mb-8 leading-relaxed">
-              👨‍🎓 MJU Convergence Software Data Technology (2022.03 ~ ) <br />
-              💻 Beyond Coding(2022.02 ~ )
-            </p>
-            <div className="flex justify-center">
-              <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                프로젝트 보러가기
-              </button>
-            </div>
-          </div>
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6"></div>
+          <Hero />
         </div>
-      </section>{" "}
+      </section>
     </Layout>
   );
 }
