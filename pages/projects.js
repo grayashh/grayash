61fe314d79a749158de4f95bdc9c97b7a2337f23
kimtaekang <k_2684@naver.com>
2022-08-27 04:41:22 +0900

    chore: add title in project page

--- pages/projects.js
+++ pages/projects.js
@@ -1,8 +1,14 @@
+import Head from "next/head";
 import Layout from "../components/layout";
 
 export default function Projects() {
   return (
     <Layout>
+      <Head>
+        <title>Grayash`s Portfolio</title>
+        <meta name="description" content="Kang's Portflolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <h1>프로젝트</h1>
     </Layout>
   );
