801d798c4f4d4ff2512339daed6c351085a210c4
kimtaekang <k_2684@naver.com>
2022-09-02 16:34:54 +0900

    build: custom 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,19 @@
+import Link from "next/link";
+import { RiAlarmWarningFill } from "react-icons/ri";
+
 export default function PageNotFound() {
   return (
-    <>
-      <h1>페이지를 찾을 수가 없습니다.</h1>
-    </>
+    <section className="bg-grey">
+      <div className="layout flex min-h-screen flex-col items-center justify-center text-center text-black">
+        <RiAlarmWarningFill
+          size={60}
+          className="hover:drop-shadow-glow animate-flicker text-red-500"
+        />
+        <h1 className="mt-8 text-4xl md:text-6xl">Page Not Found</h1>
+        <Link className="mt-4 md:text-lg" href="/">
+          Back to Home
+        </Link>
+      </div>
+    </section>
   );
 }
