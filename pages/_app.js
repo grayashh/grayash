c596dd28b4bacf11d09eb336c491f107c418487f
Grayash <k_2684@naver.com>
2022-11-17 10:21:24 +0900

    Chore : ThemeProvider 정리

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,10 @@
 import "../styles/globals.css";
-import { ThemeProvider } from "next-themes";
 import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
-      <ThemeProvider attribute="class">
         <Component {...pageProps} />
-      </ThemeProvider>
     </ChakraProvider>
   );
 }
