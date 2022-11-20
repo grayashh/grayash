11794bde6d773c9400e592330ec47b01fc849731
Grayash <k_2684@naver.com>
2022-11-20 23:41:45 +0900

    Feat : SSR false

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from "next/head";
-import Hero from "../components/Home/hero";
-import Spline from "@splinetool/react-spline";
+import dynamic from "next/dynamic";
 
 export default function Home() {
+  const NoSsr = dynamic(() => import("../components/Home/hero"), {
+    ssr: false,
+  });
+
   return (
     <>
       <Head>
@@ -11,9 +14,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
-        <Spline scene="https://prod.spline.design/3ioTEE5tnYDlyNJV/scene.splinecode" />
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <Hero />
+          <NoSsr />
         </div>
       </section>
     </>
