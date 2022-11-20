c0d61b80e8a6dd4bc89d7f5af9dea62bdf662ac7
Grayash <k_2684@naver.com>
2022-11-20 23:49:03 +0900

    Fix : local로 파일 변경

--- components/Home/hero.js
+++ components/Home/hero.js
@@ -9,7 +9,7 @@ export default function Hero() {
 
   return (
     <>
-      <NoSsr scene="https://prod.spline.design/3ioTEE5tnYDlyNJV/scene.splinecode" />
+      <NoSsr scene="/3d.splinecode" />
       <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center z-2">
         <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
           Grayash
