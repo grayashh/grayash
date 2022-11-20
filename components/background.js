b0e99c248e335c3a25f6a18be9424ca0991c457b
Grayash <k_2684@naver.com>
2022-11-21 01:09:03 +0900

    Fix : 이거 하고 안함

--- components/background.js
+++ components/background.js
@@ -1,55 +1,7 @@
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import SplineLoader from "@splinetool/loader";
+import Spline from "@splinetool/react-spline";
 
-// camera
-const camera = new THREE.PerspectiveCamera(
-  45,
-  window.innerWidth / window.innerHeight,
-  5,
-  100000
-);
-camera.position.set(385.11, -299.57, 721.56);
-camera.quaternion.setFromEuler(new THREE.Euler(0.36, 0.45, -0.16));
-
-// scene
-const scene = new THREE.Scene();
-
-// spline scene
-const loader = new SplineLoader();
-loader.load(
-  "https://prod.spline.design/3ioTEE5tnYDlyNJV/scene.splinecode",
-  (splineScene) => {
-    scene.add(splineScene);
-  }
-);
-
-// renderer
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setAnimationLoop(animate);
-document.body.appendChild(renderer.domElement);
-
-// scene settings
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFShadowMap;
-
-scene.background = new THREE.Color("#ffe8dc");
-renderer.setClearAlpha(0);
-
-// orbit controls
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.dampingFactor = 0.125;
-
-window.addEventListener("resize", onWindowResize);
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-export default function animate(time) {
-  controls.update();
-  renderer.render(scene, camera);
+export default function Background() {
+  return (
+    <Spline scene="https://prod.spline.design/3ioTEE5tnYDlyNJV/scene.splinecode" />
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import dynamic from "next/dynamic";
 import Head from "next/head";
+import { Suspense } from "react";
 import Hero from "../components/Home/hero";
 
 const Background = dynamic(() => import("../components/background"), {
   loading: () => <p>Loading...</p>,
   ssr: false,
+  suspense: true,
 });
 
 export default function Home() {
@@ -16,7 +18,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
-        <Background />
+        <Suspense fallback={`Loading...`}>
+          <Background />
+        </Suspense>
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <Hero />
         </div>
