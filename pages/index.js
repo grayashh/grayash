6757bf126a61255bbc17e8edce0d5071013e9417
Grayash <k_2684@naver.com>
2022-11-21 01:20:26 +0900

    Fix : fix

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ export default function Home() {
   const Background = dynamic(() => import("../components/background"), {
     loading: () => <p>Loading...</p>,
     ssr: false,
-    suspense: true,
   });
+
   return (
     <>
       <Head>
@@ -17,9 +17,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
-        <Suspense fallback={`Loading...`}>
-          <Background />
-        </Suspense>
+        <Background />
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <Hero />
         </div>
