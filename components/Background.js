1338fa39457046f6fd4f73d53b2bc55ae541a9b7
Grayash <k_2684@naver.com>
2022-11-20 23:07:53 +0900

    Fix : 3d 마지막 수정

--- components/Background.js
+++ components/Background.js
@@ -2,6 +2,6 @@ import Spline from "@splinetool/react-spline";
 
 export default function Background() {
   return (
-    <Spline scene="https://prod.spline.design/3ioTEE5tnYDlyNJV/scene.splinecode" />
+    <Spline scene="https://draft.spline.design/R4ih6ZGC9UYaxjKt/scene.splinecode" />
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ import Hero from "../components/Home/hero";
 export default function Home() {
   return (
     <>
+      <Background />
       <Head>
         <title>Grayash`s Portfolio</title>
         <meta name="description" content="Kang's Portflolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
-        <Background />
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <Hero />
         </div>
