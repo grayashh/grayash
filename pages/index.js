d6aac36ef4fc99228060b7d6c78cd926782201f3
Grayash <k_2684@naver.com>
2022-11-21 01:58:06 +0900

    가능?

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,8 @@
-import dynamic from "next/dynamic";
+import Spline from "@splinetool/react-spline";
 import Head from "next/head";
-import { Suspense } from "react";
 import Hero from "../components/Home/hero";
 
 export default function Home() {
-  const Background = dynamic(() => import("@splinetool/react-spline"), {
-    loading: () => <p>Loading...</p>,
-    ssr: false,
-  });
-
   return (
     <>
       <Head>
@@ -17,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
-        <Background scene="https://prod.spline.design/3ioTEE5tnYDlyNJV/scene.splinecode" />
+        <Spline scene="https://prod.spline.design/3ioTEE5tnYDlyNJV/scene.splinecode" />
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <Hero />
         </div>
