f2e339f7a0f7bc3f8d2d2d0c2fb649da95a9f18f
Grayash <k_2684@naver.com>
2022-11-20 22:42:56 +0900

    Fix : vercel 문자 인식 오류 수정

--- components/Background.js
+++ components/Background.js
@@ -1,6 +1,6 @@
 import Spline from "@splinetool/react-spline";
 
-export default function Background() {
+export default function background() {
   return (
     <Spline scene="https://prod.spline.design/3ioTEE5tnYDlyNJV/scene.splinecode" />
   );
