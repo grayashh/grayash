cc152ad0b5ec329d266916d7e7f38568954e867a
Grayash <k_2684@naver.com>
2022-11-21 02:17:52 +0900

    ff

--- pages/index.js
+++ pages/index.js
@@ -17,9 +17,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
-        <Suspense>
-          <Back scene="/3d.splinecode" />
-        </Suspense>
+        <Back scene="/3d.splinecode">
+          <Suspense />
+        </Back>
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <Hero />
         </div>
