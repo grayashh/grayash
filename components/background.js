dee342399c0990cf1d642bcb3d41f60aad1a7aa4
Grayash <k_2684@naver.com>
2022-11-21 14:24:34 +0900

    ???

--- components/background.js
+++ components/background.js
@@ -1,7 +1,5 @@
 import Spline from "@splinetool/react-spline";
 
 export default function Background() {
-  return (
-    <Spline scene="https://draft.spline.design/w5AK-DbZrRAl8qce/scene.splinecode" />
-  );
+  return <Spline />;
 }
