39e8825f90d20b2b3ad9ab3f10f576e467a5193d
Grayash <k_2684@naver.com>
2022-11-21 14:23:22 +0900

    에휴

--- components/Home/hero.js
+++ components/Home/hero.js
@@ -11,9 +11,7 @@ const DynamicBackground = dynamic(() => import("../background"), {
 export default function Hero() {
   return (
     <>
-      <Suspense fallback={<DynamicBackground />}>
-        <DynamicBackground />
-      </Suspense>
+      <DynamicBackground />
       <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center z-2">
         <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
           Grayash
