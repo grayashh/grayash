0d41d81b4aa516993f483890d24e0bbba445e9a7
Grayash <k_2684@naver.com>
2022-11-23 11:00:06 +0900

    Fix : config 수정

--- next.config.js
+++ next.config.js
@@ -3,16 +3,6 @@ const nextConfig = {
   images: {
     domains: ["www.notion.so", "images.unsplash.com"],
   },
-  webpack(config) {
-    console.log(config);
-    const prod = process.env.MODE_ENV === "production";
-    return {
-      ...config,
-      mode: prod ? "production" : "development",
-      devtool: prod ? "hidden-source-map" : "eval",
-      // 소스코드 숨기면서 에러 시 소스맵 제공 : 빠르게 웹팩 적용
-    };
-  },
 };
 
 module.exports = nextConfig;
