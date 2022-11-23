8abc62770a81bb90ac5fa4d5a2db71a3146ec127
Grayash <k_2684@naver.com>
2022-11-23 11:03:16 +0900

    Fix : promise 추가

--- components/background.js
+++ components/background.js
@@ -1,5 +1,11 @@
 import Spline from "@splinetool/react-spline";
 
+import dynamic from "next/dynamic";
+
+let DynamicSpline = dynamic(() => Promise.resolve(Spline), {
+  ssr: false,
+});
+
 export default function Back() {
-  return <Spline scene="3d.spline" />;
+  return <DynamicSpline scene="3d.spline" />;
 }
