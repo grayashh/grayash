17baa6ed7dbe28dcadf53a5d131192bb71baab22
Grayash <k_2684@naver.com>
2022-11-23 11:43:51 +0900

    Feat : iframe 수정

--- components/background.js
+++ components/background.js
@@ -5,6 +5,9 @@ export default function Back() {
       src="https://my.spline.design/glasscirclescopy-7b4f2dd37b1b740eaf2eed2e501e2386/"
       style={{
         position: "fixed",
+        zIndex: "0",
+        top: "0",
+        right: "0",
       }}
       width="100%"
       height="100%"
