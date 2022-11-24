959c44c883605a9b436316047377d5fe608cd8e2
Grayash <k_2684@naver.com>
2022-11-24 15:22:05 +0900

    Fix : 안뜨는 문제

--- pages/index.js
+++ pages/index.js
@@ -15,11 +15,11 @@ export default function Home() {
         <meta name="description" content="Kang's Portflolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Back />
       <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <Hero />
         </div>
+        <Back />
       </section>
     </>
   );
