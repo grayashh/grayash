54f7a9446eaa7adce7ad93c4c35aab20dcb731cc
Grayash <k_2684@naver.com>
2022-11-27 14:57:30 +0900

    Feat : add useEffect

--- pages/rendering.js
+++ pages/rendering.js
@@ -1,6 +1,14 @@
 import Spline from "@splinetool/react-spline";
+import { useEffect, useState } from "react";
 
 export default function Rendering() {
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
   return (
     <Spline scene="https://prod.spline.design/uAXONVPhbrhy3Tzi/scene.splinecode" />
   );
