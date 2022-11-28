0729f62e23907bb9a96bd56e3ba5d3828ffa31df
Grayash <k_2684@naver.com>
2022-11-28 10:41:00 +0900

    Chore : page에서 삭제

--- pages/rendering.js
+++
@@ -1,9 +0,0 @@
-const Spline = dynamic(() => import("@splinetool/react-spline"), {
-  ssr: false,
-});
-
-import dynamic from "next/dynamic";
-
-export default function Rendering() {
-  return <Spline scene="/scene.splinecode" />;
-}
