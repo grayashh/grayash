839150f9ab21b4c4e9f3bca47d5dc19d39fc81f7
김태강 <k_2684@naver.com>
2023-07-05 22:55:22 +0900

    feat: 최상단 layout type define

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,14 +3,16 @@ import Footer from "./footer";
 import { mergeRefs } from "react-merge-refs";
 import { forwardRef, useRef } from "react";
 
-const Layout = forwardRef(({ children, ...props }, ref) => {
-  const localRef = useRef();
-  return (
-    <div ref={mergeRefs([ref, localRef])} className="bg-primary">
-      <Header />
-      <div>{children}</div>
-      <Footer />
-    </div>
-  );
-});
+const Layout = forwardRef(
+  ({ children, ...props }: { children: React.ReactNode }, ref) => {
+    const localRef = useRef<HTMLDivElement>(null);
+    return (
+      <div ref={mergeRefs([ref, localRef])} className="bg-primary">
+        <Header />
+        {children}
+        <Footer />
+      </div>
+    );
+  }
+);
 export default Layout;
